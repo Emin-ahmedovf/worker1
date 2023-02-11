649da72048eeac0d8c7f022194d139892616ab04
Emin-ahmedovf <eminoyun45@gmail.com>
2023-02-11 17:20:23 +0300

    Delete kelime_bot directory

--- kelime_bot/helpers/keyboards.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardMarkup
-
-kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("ℹ️ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 " , url= "t.me/TheBorzMaf")]
-])
-
-
