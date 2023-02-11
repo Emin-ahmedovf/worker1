84e3111ecd9d059b7e6dcdd4131515aadfac5751
Emin-ahmedovf <eminoyun45@gmail.com>
2023-02-11 17:22:11 +0300

    Add files via upload

---
+++ Kelime_bot/__init__.py
@@ -0,0 +1,51 @@
+from time import sleep
+from pyrogram import Client
+import logging
+
+
+# THE LOGGING
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+
+
+
+
+# Hesap
+API_ID = "12675882"
+API_HASH = "a07d947d088d79d34fbe0fd95d65ef16"
+TOKEN = "5307210494:AAEtKX7hBhv3dgM2GtKMHXZR3it-F1d39qU"
+USERNAME = "Sozitapbot"
+
+
+
+
+# BOT CLIENTİ
+app = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=TOKEN,
+    plugins=dict(root="kelime_bot/plugins/"),
+    workers=16
+    )
+
+
+# Oyun Verileri
+oyun = {}
+
+
+# rating
+rating = {}
+
+
+
+
+
+# !!!!!!!!!!!!!! DEĞİŞTİR KESİNLİKLE !!!!!!!!!!!!!!!!
+#      SAHİBİN USER ID'Sİ
+OWNER_ID = 5232474605
+
